840117f54e31828af1bd1fbc88c25387ab2991e7
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2022-04-22 22:27:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,109 +1,86 @@
-import { updateGround, setupGround } from "./ground.js"
-import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
-import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
+import {
+  incrementCustomProperty,
+  setCustomProperty,
+  getCustomProperty,
+} from "./updateCustomProperty.js"
 
-const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 30
-const SPEED_SCALE_INCREASE = 0.00001
+const dinoElem = document.querySelector("[data-dino]")
+const JUMP_SPEED = 0.3
+const GRAVITY = 0.0012
+const DINO_FRAME_COUNT = 2
+const FRAME_TIME = 100
 
-const worldElem = document.querySelector("[data-world]")
-const scoreElem = document.querySelector("[data-score]")
-const startScreenElem = document.querySelector("[data-start-screen]")
-const endScreenElem = document.querySelector("[data-end-screen]")
 
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
-document.addEventListener("keydown", handleStart, { once: true })
-document.addEventListener("touchstart", handleStart, { once: true })
+let isJumping
+let dinoFrame
+let currentFrameTime
+let yVelocity
+export function setupDino() {
+  isJumping = false
+  dinoFrame = 0
+  currentFrameTime = 0
+  yVelocity = 0
+  setCustomProperty(dinoElem, "--bottom", 0)
+  document.removeEventListener("keydown", onJump)
+  document.removeEventListener("click", mobJump)
+  document.addEventListener("keydown", onJump)
+  document.addEventListener("click", mobJump)
+  
+  
+  
+  
+}
 
-let lastTime
-let speedScale
-let score
-function update(time) {
-  if (lastTime == null) {
-    lastTime = time
-    window.requestAnimationFrame(update)
+export function updateDino(delta, speedScale) {
+  handleRun(delta, speedScale)
+  handleJump(delta)
+}
+
+export function getDinoRect() {
+  return dinoElem.getBoundingClientRect()
+}
+
+export function setDinoLose() {
+  dinoElem.src = "imgs/dino-lose.png"
+}
+
+function handleRun(delta, speedScale) {
+  if (isJumping) {
+    dinoElem.src = `imgs/dino-stationary.png`
     return
   }
-  const delta = time - lastTime
 
-  updateGround(delta, speedScale)
-  updateDino(delta, speedScale)
-  updateCactus(delta, speedScale)
-  updateSpeedScale(delta)
-  updateScore(delta)
-  if (checkLose()) return handleLose()
-
-  lastTime = time
-  window.requestAnimationFrame(update)
-}
-
-function checkLose() {
-  const dinoRect = getDinoRect()
-  return getCactusRects().some(rect => isCollision(rect, dinoRect))
-
-}
-
-function isCollision(rect1, rect2) {
-  return (
-    rect1.left < rect2.right &&
-    rect1.top < rect2.bottom &&
-    rect1.right > rect2.left &&
-    rect1.bottom > rect2.top
-  )
-  
-}
-
-function updateSpeedScale(delta) {
-  speedScale += delta * SPEED_SCALE_INCREASE
-}
-
-function updateScore(delta) {
-  score += delta * 0.01
-  scoreElem.textContent = Math.floor(score)
-}
-
-function handleStart() {
-  lastTime = null
-  speedScale = 1
-  score = 0
-  setupGround()
-  setupDino()
-  setupCactus()
-  startScreenElem.classList.add("hide")
-  window.requestAnimationFrame(update)
-}
-
-function handleLose() {
-  setDinoLose()
-
-  setTimeout(() => {
-    document.addEventListener("keydown", "touchstart", handleStart, { once: true })
-    endScreenElem.classList.remove("hide")
-  }, 100)
-      document.addEventListener("touchstart", "touchstart", handleStart, { once: true })
-    endScreenElem.classList.remove("hide")
-  }, 100)
-
-  function soundEnd(){
-    var audio = new Audio('War.mp3');
-    audio.play();
+  if (currentFrameTime >= FRAME_TIME) {
+    dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
+    dinoElem.src = `imgs/dino-run-${dinoFrame}.png`
+    currentFrameTime -= FRAME_TIME
   }
-  soundEnd()
-
-  
-
-
+  currentFrameTime += delta * speedScale
 }
 
-function setPixelToWorldScale() {
-  let worldToPixelScale
-  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
-  } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+function handleJump(delta) {
+  if (!isJumping) return
+
+  incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
+
+  if (getCustomProperty(dinoElem, "--bottom") <= 0) {
+    setCustomProperty(dinoElem, "--bottom", 0)
+    isJumping = false
   }
 
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+  yVelocity -= GRAVITY * delta
+}
+
+function onJump(e) {
+  if (e.code !== "Space" || isJumping) return
+
+  yVelocity = JUMP_SPEED
+  isJumping = true
+}
+
+function mobJump(){
+
+  yVelocity = JUMP_SPEED
+  isJumping = true
+
 }
